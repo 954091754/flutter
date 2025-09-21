d99c503cb647602aacb9781f3b3156793cc68e10
Jesion <954091754@qq.com>
2025-09-21 19:40:15 +0800

    change tabbar item content

--- galaxy/lib/pages/root/root_page.dart
+++ galaxy/lib/pages/root/root_page.dart
@@ -12,9 +12,10 @@ class _RootPageState extends State<RootPage> {
   int _currentIndex = 0;
 
   final List<Widget> _pages = [
-    const Center(child: Text('微信', style: TextStyle(fontSize: 20))),
+    const Center(child: Text('首页', style: TextStyle(fontSize: 20))),
     const MediaPage(),
-    const Center(child: Text('发现', style: TextStyle(fontSize: 20))),
+    const Center(child: Text('小组', style: TextStyle(fontSize: 20))),
+    const Center(child: Text('市集', style: TextStyle(fontSize: 20))),
     const Center(child: Text('我', style: TextStyle(fontSize: 20))),
   ];
 
@@ -31,9 +32,10 @@ class _RootPageState extends State<RootPage> {
         selectedLabelStyle: const TextStyle(fontSize: 12),
         unselectedLabelStyle: const TextStyle(fontSize: 12),
         items: const [
-          BottomNavigationBarItem(icon: Icon(Icons.chat), label: '微信'),
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: '首页'),
           BottomNavigationBarItem(icon: Icon(Icons.library_books), label: '书影音'),
-          BottomNavigationBarItem(icon: Icon(Icons.explore), label: '发现'),
+          BottomNavigationBarItem(icon: Icon(Icons.group), label: '小组'),
+          BottomNavigationBarItem(icon: Icon(Icons.store), label: '市集'),
           BottomNavigationBarItem(icon: Icon(Icons.person), label: '我'),
         ],
       ),
